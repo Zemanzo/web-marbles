f799577762eb0411dad08f4d51eaaca7ef452d0b
Merlijn van der Kamp <zemanzo.my@gmail.com>
2018-04-20 03:03:57 +0200

    Cleared out old code

--- web-marbles.js
+++ web-marbles.js
@@ -6,23 +6,14 @@ console.log("   by "+"Z".green+"emanz"+"o".green);
 console.log(" "+(new Date()).toLocaleString('nl').cyan);
 
 const CANNON = require('cannon');
-
-var size = .1;
-var interval, physStepInterval;
-
-// Spheres
 var world = new CANNON.World();
-
 world.gravity.set(0,0,-50);
 world.broadphase = new CANNON.NaiveBroadphase();
 world.solver.iterations = 5;
-
 world.defaultContactMaterial.contactEquationStiffness = 5e6;
 world.defaultContactMaterial.contactEquationRelaxation = 10;
-
-/* // Since we have many bodies and they don't move very much, we can use the less accurate quaternion normalization
-world.quatNormalizeFast = true;
-world.quatNormalizeSkip = 4; // ...and we do not have to normalize every step. */
+world.quatNormalizeFast = true; // Since we have many bodies and they don't move very much, we can use the less accurate quaternion normalization
+world.quatNormalizeSkip = 4; // ...and we do not have to normalize every step.
 
 // ground plane
 var groundShape = new CANNON.Plane(new CANNON.Vec3(0,0,1));
@@ -31,16 +22,9 @@ groundBody.addShape(groundShape);
 groundBody.position.set(0,0,-1000);
 world.add(groundBody);
 
-/* var obj = new OBJFile(fs.readFileSync("resources/mapObj.obj","utf-8"));
-var heightMap = obj.parse();
-var zVertices = Array.from(heightMap.models[0].vertices, x => x.x);
-var distanceBetween = Math.abs(heightMap.models[0].vertices[1].x - heightMap.models[0].vertices[0].x); */
-
 // Load obj as heightfield
 const OBJHeightfield = require('./src/model-import/obj-heightfield');
 var mapObj = new OBJHeightfield("map2.obj");
-
-/* console.log(mapObj.heightArray); */
 
 // Create the heightfield
 var hfShape = new CANNON.Heightfield(mapObj.heightArray, {
@@ -50,38 +34,6 @@ var hfBody = new CANNON.Body({ mass: 0 });
 hfBody.addShape(hfShape);
 hfBody.position.set(-25, -25, -10);
 world.add(hfBody);
-
-// Add spheres
-/* for(var i=0; i<15 - 1; i++){
-	for (var j = 0; j < 15 - 1; j++) {
-		if(i===0 || i >= 15-2 || j===0 || j >= 15-2)
-			continue;
-		var sphereShape = new CANNON.Sphere(0.3);
-		var sphereBody = new CANNON.Body({ mass: 1 });
-		sphereBody.addShape(sphereShape);
-		sphereBody.linearDamping = 0.2;
-		sphereBody.position.set(10.25 + i, 10.25 + j, 50);
-		sphereBody.position.vadd(hfBody.position, sphereBody.position);
-		world.addBody(sphereBody);
-	}
-} */
-
-/* var bodies = [];
-var i = 0;
-interval = setInterval(function(){
-	// Sphere
-	i++;
-	var sphereShape = new CANNON.Sphere(size);
-	var b1 = new CANNON.Body({ mass: 5 });
-	b1.addShape(sphereShape);
-	b1.position.set(Math.random(),Math.random(),30);
-	world.add(b1);
-	bodies.push(b1);
-},500);
-
-setTimeout(function(){
-	clearInterval(interval)
-},10050); */
 
 // Express connections
 var express = require('express');
@@ -166,21 +118,6 @@ io.on('connection', function(socket){
 io.on('disconnect', function(socket){
 	console.log('A user disconnected...'.red);
 });
-
-/* let pos;
-physSocketEmitInterval = setInterval(function(){
-	pos = new Float32Array(world.bodies.length*3);
-	let i = 0;
-	for (key in world.bodies){
-		pos[i+0] = world.bodies[key].position.x;
-		pos[i+1] = world.bodies[key].position.y;
-		pos[i+2] = world.bodies[key].position.z;
-		i+=3;
-	}
-	io.emit('physics step', pos.buffer);
-},1000/config.network.tickrate); */
-
-
 
 /* Physics interval */
 physStepInterval = setInterval(function(){
