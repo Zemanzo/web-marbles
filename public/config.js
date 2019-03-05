e49e27a75ede2323e2dba8c2fe2f349cafd04068
Merlijn van der Kamp <zemanzo.my@gmail.com>
2019-03-05 14:12:49 +0100

    Secure websockets

--- public/config.js
+++ public/config.js
@@ -1,19 +1,20 @@
-export let controls = {
-		camera: {
-			speed: 150
-		}
-	},
-	network = {
-		tickrate: 10,
-		ticksToLerp: 2
-	},
-	graphics = {
-		castShadow: {
-			marbles: true
-		},
-		receiveShadow: {
-			marbles: false
-		}
-	};
+const config = {};
 
+config.controls = {};
+config.controls.camera = {};
+config.controls.camera.speed = 150;
 
+config.network = {};
+config.network.tickrate = 10;
+config.network.ticksToLerp = 2;
+config.network.websockets = {};
+config.network.websockets.port = 3014;
+config.network.websockets.ssl = false;
+
+config.graphics = {};
+config.graphics.castShadow = {};
+config.graphics.castShadow.marbles = true;
+config.graphics.receiveShadow = {};
+config.graphics.receiveShadow.marbles = false;
+
+module.exports = config;
