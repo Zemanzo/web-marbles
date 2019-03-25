b1be787d463ad26d0640ae429adec1e2f04c3a19
Merlijn van der Kamp <zemanzo.my@gmail.com>
2019-03-25 09:53:48 +0100

    Added client user config overrides; Fixed falsey config properties being incorrectly disregarded as non-existing

--- src/client/config.js
+++ src/client/config.js
@@ -1,3 +1,8 @@
+// Note: Do NOT modify this config file directly!
+// If you want to override any settings,
+// please do so in config.user.js!
+
+const userConfig = require("./config.user");
 const config = {};
 
 config.controls = {};
@@ -17,5 +22,31 @@ config.graphics.castShadow = {};
 config.graphics.castShadow.marbles = true;
 config.graphics.receiveShadow = {};
 config.graphics.receiveShadow.marbles = false;
+
+/* Override any user properties set in config.user.js */
+for(let key in userConfig) {
+	let obj = config;
+	let property;
+
+	key.split(".").forEach( function(val) {
+		if(obj) {
+			if(typeof obj[val] !== "undefined") {
+				if(typeof obj[val] === "object") {
+					obj = obj[val]; // change to child object
+				} else {
+					property = val; // obj[property] is the property to set
+				}
+			} else {
+				obj = null; // Property doesn't exist
+			}
+		}
+	} );
+
+	if(obj && property) {
+		obj[property] = userConfig[key];
+	} else {
+		console.warn(`Warning: Cannot override non-existing config property: config.${key}`);
+	}
+}
 
 module.exports = config;
---
+++ src/client/config.user.example.js
@@ -0,0 +1,9 @@
+// Add any config properties you wish to override here
+// To see which properties you can override, check config.js
+
+const userConfig = {
+	"network.ssl": false,
+	"network.websockets.localReroute": false
+};
+
+module.exports = userConfig;
