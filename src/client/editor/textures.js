2a4c44bc16cee91ae7cc2dc44218657450039887
Merlijn van der Kamp <zemanzo.my@gmail.com>
2019-05-06 17:16:57 +0200

    Ability to add custom materials & textures to models (WIP)

---
+++ src/client/editor/textures.js
@@ -0,0 +1,100 @@
+import * as THREE from "three";
+import { modelsTab } from "./models";
+import { projectTab } from "./project";
+import { editorLog } from "./log";
+
+// texture object
+function Texture(name, texture, projectData) {
+	this.name = name;
+	this.projectData = projectData; // Project reference for this texture
+	this.element = null;
+	this.prefabEntities = {};
+
+	// Deep clone for editor
+	this.element = document.getElementById("textureTemplate").cloneNode(true);
+
+	// Add to texture list
+	this.element.id = name;
+	let imageElement = this.element.getElementsByClassName("image")[0];
+	imageElement.src = texture;
+	this.element.getElementsByClassName("name")[0].innerText = name;
+
+	// Since THREE.Texture requires a texture as a HTML element, we re-use the one from the list
+	this.map = new THREE.Texture(imageElement);
+
+	// Delete texture button
+	this.element.getElementsByClassName("delete")[0].addEventListener("click", () => {
+		if (confirm(`Are you sure you want to delete texture ${name}?`)) {
+			texturesTab.removeTexture(name);
+		}
+	}, false);
+
+	// Add to DOM
+	this.element = document.getElementById("textures").appendChild(this.element);
+}
+
+let texturesTab = function() {
+	return {
+		textures: {},
+
+		initialize: function() {
+			// Add textures button
+			document.getElementById("addTexture").addEventListener("click", function() { document.getElementById("addTextureFile").click(); }, false);
+			document.getElementById("addTextureFile").addEventListener("change", function() {
+				Array.from(this.files).forEach(function(file) {
+					if (file.name in projectTab.activeProject.textures) {
+						editorLog(`Texture ${file.name} already loaded.`, "warn");
+						return;
+					}
+
+					if (file.name === "null") {
+						editorLog("Nice try.", "success");
+						return;
+					}
+
+					file.reader = new FileReader();
+					file.reader.onload = function() {
+						// Attempt to load texture and add it to the project
+						let project = projectTab.activeProject.addTexture(file.name, file.reader.result);
+						texturesTab.textures[file.name] = new Texture(file.name, file.reader.result, project);
+					};
+
+					file.reader.onerror = function() {
+						editorLog(`Unable to load texture (${file.name}): ${file.reader.error.message}`, "error");
+						file.reader.abort();
+					};
+
+					file.reader.readAsDataURL(file);
+				});
+			}, false);
+		},
+
+		onTabActive: function() {
+			modelsTab.group.visible = true;
+		},
+
+		onTabInactive: function() {
+			modelsTab.group.visible = false;
+		},
+
+		removeTexture: function(name) {
+			if (name in this.textures === false) {
+				console.log(`Attempted to remove texture ${name}, but no such texture exists!`);
+				return;
+			}
+
+			let thisTexture = this.textures[name];
+
+			// Remove from editor
+			thisTexture.element.parentNode.removeChild(thisTexture.element);
+			delete this.textures[name];
+
+			// Remove from project
+			delete projectTab.activeProject.textures[name];
+
+			editorLog(`Removed texture: ${name}`, "info");
+		}
+	};
+}();
+
+export { texturesTab };
