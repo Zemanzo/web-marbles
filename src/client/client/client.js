c0d3bcb91e9d5ce81d70c79ec718c8c9151305db
Angelo <30075631+Angelovandermark@users.noreply.github.com>
2019-05-29 20:10:19 +0200

    Modulified and cleaned up networking for client. Game.js now handles it's game state initialization.

--- src/client/client/client.js
+++ src/client/client/client.js
@@ -1,43 +1,14 @@
-import domReady from "../dom-ready";
 import { renderCore } from "../render/render-core";
-import "./render";
-import { net as networking } from "./networking";
-import { game } from "./game";
+import "./render"; // "Initialization" aka temporary function override
 import { levelManager } from "../level-manager";
-import * as levelIO from "../../level/level-io";
+import domReady from "../dom-ready";
+import { game } from "./game";
+import { networking } from "./networking";
 
 // Initialize client modules
 renderCore.initialize();
 levelManager.initialize();
 domReady.then( () => {
-	// Coming soon
-	//game.initialize();
-	//networking.initialize();
-});
-
-// If both promises fulfill, start rendering & fill entries field
-Promise.all([networking.socketReady, domReady]).then(() => {
-	for (let i = 0; i < networking.marbleData.length; i++) {
-		game.spawnMarble(networking.marbleData[i]);
-	}
-});
-
-// Level loading and initialisation
-networking.socketReady.then((initialData) => {
-	let levelName = initialData.levelId;
-
-	fetch(`/resources/maps/${levelName}.mmc`)
-		.then((response) => {
-			// Return as a buffer, since .text() tries to convert to UTF-8 which is undesirable for compressed data
-			return response.arrayBuffer();
-		})
-		.then((buffer) => {
-			let levelData = levelIO.load(buffer);
-			levelManager.activeLevel.loadLevel(levelData)
-				.then( () => {
-					if(game.getCurrentGameState() === "started") {
-						levelManager.activeLevel.openGates();
-					}
-				});
-		});
+	game.initialize();
+	networking.initialize();
 });
--- src/client/client/networking.js
+++ src/client/client/networking.js
@@ -4,95 +4,86 @@ import { TypedSocketHelper } from "./typed-socket-helper";
 import { HUDNotification } from "./hud-notification";
 import { game } from "./game";
 
-let wsUri = `ws${config.ssl ? "s" : ""}://${window.location.hostname}${config.websockets.localReroute ? "" : `:${config.websockets.port}`}/ws/gameplay`;
-let ws = new ReconnectingWebSocket(wsUri, [], {
-	minReconnectionDelay: 1000,
-	maxReconnectionDelay: 30000,
-	reconnectionDelayGrowFactor: 2
-});
+let networking = function() {
+	let _wsUri = `ws${config.ssl ? "s" : ""}://${window.location.hostname}${config.websockets.localReroute ? "" : `:${config.websockets.port}`}/ws/gameplay`;
+	let _ws = null;
+	let _helper = null;
 
-ws.addEventListener("open", function() {
-	net.websocketOpen = true;
-	net.ready = 0;
-});
-
-ws.addEventListener("close", function() {
-	net.websocketOpen = false;
-});
-
-let helper = new TypedSocketHelper("/gameplay");
-
-let net = { // Initialize, do not configure these values.
-	marbleData: undefined,
-	marblePositions: new Float32Array(0),
-	marbleRotations: new Float32Array(0),
-	lastUpdate: 0,
-	ready: 0,
-	requestsSkipped: 0, // Helps detect network issues
-	websocketOpen: false
-};
-
-// Socket data promise
-net.socketReady = new Promise((resolve) => {
-	// Once connected, client receives initial data
-	ws.addEventListener("message", function(event) {
-		let { type, message } = helper.extractSocketMessageType(event.data);
+	let _processMessageEvent = function(event) {
+		let { type, message } = _helper.extractSocketMessageType(event.data);
+		message = JSON.parse(message);
 
 		switch(type) {
 		case "initial_data":
-			message = JSON.parse(message);
-
-			// Initial marble sizes, colors and names
-			net.marbleData = message.initialMarbleData;
-
-			// Unused anywhere else, cleaner to get rid of it now.
-			delete message.initialMarbleData;
-
-			game.setInitialGameState(message);
-
-			resolve(message);
+			game.initializeGameState(message);
+			_requestPhysics(); // Start the request physics loop
 			break;
 		case "request_physics":
-			message = JSON.parse(message);
-			if (message) {
-				net.marblePositions = new Float32Array(Object.values(message.pos));
-				net.marbleRotations = new Float32Array(Object.values(message.rot));
+			if (message) { // False if there is no data to process
+				networking.marblePositions = new Float32Array(Object.values(message.pos));
+				networking.marbleRotations = new Float32Array(Object.values(message.rot));
 			}
-			net.lastUpdate = 0;
-			net.ready--;
+			networking.lastUpdate = 0;
+			networking.ready--;
 			break;
 		case "new_marble":
-			game.spawnMarble(JSON.parse(message));
+			game.spawnMarble(message);
 			break;
 		case "finished_marble":
-			game.finishMarble(JSON.parse(message));
+			game.finishMarble(message);
 			break;
 		case "state":
-			game.setCurrentGameState(JSON.parse(message));
+			game.setCurrentGameState(message);
 			break;
 		case "notification":
-			message = JSON.parse(message);
 			new HUDNotification(message.content, message.duration, message.style);
 			break;
 		}
-	});
-}).then((message) => {
-	/* Physics syncing */
-	// Once connection is acknowledged, start requesting physics updates
-	let getServerData = function() {
-		if (net.ready < config.tickrate && net.websocketOpen) {
-			net.ready++;
-			ws.send(
-				helper.addMessageType(Date.now().toString(), "request_physics")
-			);
-		} else if (net.websocketOpen) {
-			net.requestsSkipped++;
-		}
-		setTimeout(getServerData, 1000 / config.tickrate);
 	};
-	getServerData();
 
-	return message;
-});
+	let _requestPhysics = function() {
+		if (networking.ready < config.tickrate && networking.websocketOpen) {
+			networking.ready++;
+			_ws.send(
+				_helper.addMessageType(Date.now().toString(), "request_physics")
+			);
+		} else if (networking.websocketOpen) {
+			networking.requestsSkipped++;
+		}
+		setTimeout(_requestPhysics, 1000 / config.tickrate);
+	};
 
-export { net, ws };
+	return {
+		marblePositions: new Float32Array(0),
+		marbleRotations: new Float32Array(0),
+		lastUpdate: 0,
+		ready: 0,
+		requestsSkipped: 0, // Helps detect network issues
+		websocketOpen: false,
+
+		initialize: function() {
+			_ws = new ReconnectingWebSocket(_wsUri, [], {
+				minReconnectionDelay: 1000,
+				maxReconnectionDelay: 30000,
+				reconnectionDelayGrowFactor: 2
+			});
+
+			_ws.addEventListener("open", () => {
+				this.websocketOpen = true;
+				this.ready = 0;
+			});
+
+			_ws.addEventListener("close", () => {
+				this.websocketOpen = false;
+			});
+
+			_ws.addEventListener("message", (event) => {
+				_processMessageEvent(event);
+			});
+
+			_helper = new TypedSocketHelper("/gameplay");
+		}
+	};
+}();
+
+export { networking };
