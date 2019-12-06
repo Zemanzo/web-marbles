7c618b9575a8fe176f4ef770621ba160667756df
Angelo <30075631+Qaomen@users.noreply.github.com>
2019-12-06 23:45:35 +0100

    Bumped up editor version number for GLB support.

--- src/level/level-data.js
+++ src/level/level-data.js
@@ -1,7 +1,7 @@
 function LevelData() {
 	this.levelName = "New level";
 	this.authorName = "Unknown";
-	this.version = "0.3.0";
+	this.version = "0.3.1";
 	this.type = "project";
 	this.exportDate = 0;
 
