9f8c897a1c45eec61072facbb91ac32086490ef5
Merlijn van der Kamp <zemanzo.my@gmail.com>
2020-05-09 14:10:09 +0200

    Improved classname for content revealer

--- src/components/content-revealer.js
+++ src/components/content-revealer.js
@@ -3,7 +3,7 @@ import React from "react";
 const ContentRevealer = (props) => {
 	const [contentIsRevealed, setContentIsRevealed] = React.useState(false);
 	const handleButtonClick = () => setContentIsRevealed(true);
-	const className = `contentRevealer ${contentIsRevealed ? false : "reveal"}`;
+	const className = `contentRevealer${contentIsRevealed ? " revealed" : " unrevealed"}`;
 	return (
 		<div className={className} onClick={handleButtonClick}>
 			{contentIsRevealed ? props.newContent : props.initialContent}
